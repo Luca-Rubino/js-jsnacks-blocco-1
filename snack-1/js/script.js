1657a32b0765fa5dd6cb2eabacaacebd0d32ae7e
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-13 11:58:06 +0200

    svolgimento richiesta

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -2,3 +2,12 @@ console.log('hi!')
 
 // L'utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
+
+num1 = parseInt(prompt('scrivi un numero'))
+num2 = parseInt(prompt('scrivi un numero'))
+
+if (num1 > num2){
+    console.log(num1)
+} else {
+    console.log(num2)
+}
