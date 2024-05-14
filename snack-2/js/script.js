d461d06e77c0c996c8d2a47fd372d37989ca5635
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-14 10:58:57 +0200

    Svolgere esercizio in maniera consona dopo la correzione

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -2,7 +2,13 @@ console.log('Hi!')
 
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
-const numberList = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5]
+let somma=0
+for(let index=1; index < numbers.langth;index++){
+    if (index % 2 === 1){
+        somma+=numbers[index]
+    }
+}
 
 
-console.log (2 + 4)
+console.log (numbers, somma)
