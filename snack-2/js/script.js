b5578389ccccf22ea888d59950bff39f7b3f8cae
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-14 11:27:19 +0200

    Svolgimento esercizio dopo la correzione

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -16,6 +16,22 @@ console.log (numbers, somma)
 
 // Fai inserire un numero, che chiameremo N, all'utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 
-let n = prompt('Inserisci un numero');
+// let n = prompt('Inserisci un numero');
 
-console.log(([nArray = Math.floor(Math.random())*10+1])*n);
+// console.log(([nArray = (Math.floor(Math.random()))*10+1])*n);
+
+let userNumber = Number.parseInt(prompt('Inserisci un numero'),10);
+if (userNumber > 20 || userNumber < 1 || Number.isNaN(userNumber)){
+    userNumber=12;
+    console.warn('numero non valido')
+}
+
+for (let index=0; index<userNumber;index++){
+    const tempArray = [];
+
+    for (let k=0; k<10; k++){
+        tempArray.push(Math.floor(Math.random() * 100) +1);
+    }
+
+    console.log(tempArray)
+}
