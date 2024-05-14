bb9b08f9345fc24402c547f517755b46ccdc3569
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-14 11:43:31 +0200

    Tentare di realizare funzione per la creazione elementi

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -2,16 +2,16 @@ console.log('Hi!')
 
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
-const numbers = [1, 2, 3, 4, 5]
-let somma=0
-for(let index=1; index < numbers.langth;index++){
-    if (index % 2 === 1){
-        somma+=numbers[index]
-    }
-}
+// const numbers = [1, 2, 3, 4, 5]
+// let somma=0
+// for(let index=1; index < numbers.langth;index++){
+//     if (index % 2 === 1){
+//         somma+=numbers[index]
+//     }
+// }
 
 
-console.log (numbers, somma)
+// console.log (numbers, somma)
 
 
 // Fai inserire un numero, che chiameremo N, all'utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
@@ -20,27 +20,32 @@ console.log (numbers, somma)
 
 // console.log(([nArray = (Math.floor(Math.random()))*10+1])*n);
 
-let userNumber = Number.parseInt(prompt('Inserisci un numero'),10);
-if (userNumber > 20 || userNumber < 1 || Number.isNaN(userNumber)){
-    userNumber=12;
-    console.warn('numero non valido')
-}
+// let userNumber = Number.parseInt(prompt('Inserisci un numero'),10);
+// if (userNumber > 20 || userNumber < 1 || Number.isNaN(userNumber)){
+//     userNumber=12;
+//     console.warn('numero non valido')
+// }
 
-for (let index=0; index<userNumber;index++){
-    const tempArray = [];
+// for (let index=0; index<userNumber;index++){
+//     const tempArray = [];
 
-    for (let k=0; k<10; k++){
-        tempArray.push(Math.floor(Math.random() * 100) +1);
-    }
+//     for (let k=0; k<10; k++){
+//         tempArray.push(Math.floor(Math.random() * 100) +1);
+//     }
 
-    console.log(tempArray)
-}
+//     console.log(tempArray)
+// }
 
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
 
-const primoDiv = (Document.createElement("div#primo"));
 
-primoDiv.innerHTML = "Primo";
-const secondoDiv = (Document.createElement("div#secondo"));
+function innerHTML(){
+    const primoDiv = (Document.createElement("div#primo"));
 
-secondoDiv.innerHTML = "Primo";
+    primoDiv.innerHTML = "Primo";
+
+    const secondoDiv = (Document.createElement("div#secondo"));
+
+    secondoDiv.innerHTML = "Secondo";
+}
+
