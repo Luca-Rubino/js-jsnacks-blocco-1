3d1d22a001aae15b5d733ca806e8f262a9a70e94
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-21 12:41:41 +0200

    Svolgere esercizio slack 14

---
+++ slack-14/js/script.js
@@ -0,0 +1,21 @@
+console.log('slack-14')
+
+// Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
+
+
+let userNumber = parseInt(prompt('Quanti secondi deve avere il timer?'),10)
+console.log(userNumber)
+
+
+setInterval(() => {
+    
+}, 100);
+
+
+setTimeout(timer, 1000)
+
+function timer(){
+    for(let i=0;i >= userNumber; i--){
+        console.log(i)
+    }
+}
